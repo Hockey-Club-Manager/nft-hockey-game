f151abe336348e9c2bc75b02c90e8c64f9861036
Alex Pitenko <oleksii.pitenko@itsyndicate.org>
2022-02-01 23:40:59 +0200

    refactored probability_of_actions(). Ref #1

--- src/player_field.rs
+++ src/player_field.rs
@@ -1,6 +1,6 @@
 use crate::player::{Action, Player, PlayerPosition, PlayerRole};
 use crate::player::ActionType::{Dangle, Move, Pass, Shot};
-use crate::player::PlayerRole::{Goon, Passer, Professor, Shooter, ToughGuy, TryHarder};
+use crate::player::PlayerRole::{Goon, Passer, Professor, Shooter, ToughGuy, TryHarder, Rock, Dangler};
 
 // #[derive(BorshDeserialize, BorshSerialize)]
 pub struct FieldPlayerStats {
@@ -53,22 +53,17 @@ impl FieldPlayer {
     }
 
     fn probability_of_actions(&self) -> Vec<Action> {
-        if self.role == Passer || self.role == Professor {
-            let result:Vec<u8> = vec![4, 1, 3, 2];
-            return to_action(result);
-        }
-        if self.role == Shooter || self.role == ToughGuy {
-            let result:Vec<u8> = vec![2, 4, 1, 3];
-            return to_action(result);
-        }
-        if self.role == TryHarder || self.role == Goon {
-            let result:Vec<u8> = vec![3, 2, 4, 1];
-            return to_action(result);
-        }
-        else {
-            let result:Vec<u8> = vec![1, 3, 2, 4];
-            return to_action(result);
-        }
+        return match self.role {
+            Passer => to_action(4, 1, 3, 2),
+            Professor => to_action(4, 1, 3, 2),
+            Shooter => to_action(2, 4, 1, 3),
+            ToughGuy => to_action(2, 4, 1, 3),
+            TryHarder => to_action(3, 2, 4, 1),
+            Goon => to_action(3, 2, 4, 1),
+            Dangler => to_action(1, 3, 2, 4),
+            Rock => to_action(1, 3, 2, 4),
+            _ => panic!()
+        };
     }
 }
 
@@ -79,23 +74,23 @@ impl Player for FieldPlayer {
     fn get_holds_puck(&self) -> bool { self.holds_puck }
 }
 
-fn to_action(actions: Vec<u8>) -> Vec<Action> {
+fn to_action(pass_probability: u8, shot_probability: u8, move_probability: u8, dangle_probability: u8) -> Vec<Action> {
     let result:Vec<Action> = vec![
         Action {
             type_action: Pass,
-            probability: actions[0],
+            probability: pass_probability,
         },
         Action {
             type_action: Shot,
-            probability: actions[1],
+            probability: shot_probability,
         },
         Action {
             type_action: Move,
-            probability: actions[2],
+            probability: move_probability,
         },
         Action {
             type_action: Dangle,
-            probability: actions[3],
+            probability: dangle_probability,
         }
     ];
 
