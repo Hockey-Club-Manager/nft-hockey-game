fd342c4df61d20d343de990147eba6586654a981
yurii <stolbetskiy.yura@gmail.com>
2022-02-03 17:06:52 +0200

    other_positions() get_random_position()

--- src/game.rs
+++ src/game.rs
@@ -5,13 +5,14 @@ use crate::user::User;
 extern crate rand;
 
 use rand::Rng;
-use crate::player::{ActionType};
+use crate::player::{ActionType, Player, PlayerPosition};
 use crate::player::ActionType::{Battle, Dangle, Move, Pass, Shot};
+use crate::player::PlayerPosition::{Center, LeftDefender, LeftWing, RightDefender, RightWing};
 
 // #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault, Clone)]
 pub struct UserInfo {
     user: User,
-    field_players: Vec<FieldPlayer>,
+    field_players: HashMap<PlayerPosition, FieldPlayer>,
     goalie: Goalie,
     // pub(crate) account_id: AccountId,
 }
@@ -39,6 +40,28 @@ pub struct Game {
 }
 
 impl Game {
+    fn get_random_position(&self, player_pos: PlayerPosition) -> PlayerPosition {
+        let player_positions = self.other_positions(player_pos);
+
+        let mut rng = rand::thread_rng();
+        let random_pos = rng.gen_range(0, 5);
+
+        player_positions[random_pos]
+    }
+
+    fn other_positions(&self, player_pos: PlayerPosition) -> Vec<PlayerPosition> {
+        let mut player_positions = vec![RightWing, LeftWing, Center, RightDefender, LeftDefender];
+
+        for num in 0..5 {
+            if player_pos == player_positions[num] {
+                player_positions.remove(num);
+                break;
+            }
+        }
+
+        player_positions
+    }
+
     fn pass(&mut self, competitor: FieldPlayer) {
         let mut rng = rand::thread_rng();
         let random_number = rng.gen_range(1, 101);
@@ -47,7 +70,7 @@ impl Game {
             if self.player_with_puck.as_ref()
                                     .unwrap()
                                     .won_pass(competitor.stats.get_strength()) {
-                // TODO
+
                 self.last_action = Pass;
 
             } else {
